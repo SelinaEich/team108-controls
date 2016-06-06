ca2a66979468caaa53ac1806f4419ff865ec1a8d
patrickz12 <patrickzhu12@gmail.com>
2016-06-06 12:44:32 -0700

    Code Fix
    A lot of errors were coming up when compiling and decompiling a lot of
    stuff. I rearranged some stuff (which to be honest may not have done
    anything), and changed some syntax stuff. I really just verified the
    code without the potentiometer part until the code compiled without any
    errors. In the current form of the code it should do that.
    
    Important to note that I did change the name for servoBearing back to
    servoBearing to avoid a redefinition error but then commented out the
    section of the code that uses that variable. This was to avoid any weird
    definition errors from popping up.  servoBearing should be defined as
    phi_desired and only defined once (it currently would be defined in both
    the Target Variables section as well as the servo variables section).
    The old servo code also needs to be reworked or changed altogether since
    it will cause a conflicting error in the code.
    
    In addition, the code relies on the presumption that the variables
    startingPosX and startingPosY have been defined in the initial setup
    loop (I have already defined them to test for that error). This means
    that they just need to be defined in the setup() loop with the
    potentiometer function.

--- ControlsVerification_108/ControlsVerification_108.ino
+++ ControlsVerification_108/ControlsVerification_108.ino
@@ -23,13 +23,13 @@ float phi;                        // magnetometer angle in PCB plane (around Z-a
 float thetaOffset = -139;             // a value of -139 makes 0 degrees represent straight down the center of the channel
 /*update thetaOffset*/
 
-float servoBearing = 90;          // where you want the robot to point
-float servoAngle;                 // desired change in servo angle
+float servoBearing;          // originally as servoBearing; has been changed to phi_desired to represent the desired ORIENTATION of the robot
+float servoAngle;                 // desired change in servo angle; should be changed to theta_target as that is representative of the desired change in the robot's steering angle
 
 /*Servo Variables*/
 int servoPosition = 1;
 unsigned long lastTimeServo;
-int servoInterval = 500; // (in milliseconds) sets time interval at which servo adjusts
+int servoInterval = 200; // (in milliseconds) sets time interval at which servo adjusts
 
 /*Servo Position Btwn 0 & 180*/
 int minServoPosition = 0;
@@ -80,11 +80,13 @@ float p_desiredX; // desired x-position
 float p_desiredY; // desired y-position
 float phi_desired; // desired orientation of robot
 float theta_target; // desired change in steering angle
+float target_lead = 33; // leading distance for channel control
 
 /*Error Variables*/
 
 float error_pX;// x-position error
 float error_pY; // y-position error
+float error_p_distance; // magnitude of position error
 float error_phi; // error between desired and current orientations
 float p_angle; // direction from robot to target (NEW)
 float alpha_p; // error between orientation (theta) and error angle (p_angle) (NEW)
@@ -100,7 +102,12 @@ int startingBlock; // notes starting block position 0 or 1
 /******************************* setup *************************************/
 void setup() {
 
-   /* whatever function that will essentially output the starting position should go here so that the variable declaration in the next two lines of code works correctly */
+  /* whatever function that will essentially output the starting position should go here so that the variable declaration in the next two lines of code works correctly */
+  /* These comments can be undone and made usable to test the controller law
+   */ startingPosX = -99;
+  startingPosY = 154;
+   
+  
 
   K_alpha_p = 1.5;
   K_phi = 1;
@@ -110,7 +117,7 @@ void setup() {
   
   originX = 0;
   originY = 0;
-  finish_lineX = 530;
+  finish_lineX = 528;
   finish_lineY = 0;
   
   pinMode (buttPin,INPUT_PULLUP); //reads 5V until button is pressed again, note: logic is inverted
@@ -137,6 +144,69 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(Switch),debounceInterrupt,FALLING);
   pinMode(PotPin, INPUT);
 }
+/************************* position estimate/distance traveled update *************************************/
+void estimate(){
+  /*assume deltaSw is >>1, therefore neglect curvature and use trig. to estimate x & y distances traveled*/
+  distanceX += cos(phi) * deltaSw; // updates distance traveled in x-direction since last reed trigger
+  distanceY += sin(phi) * deltaSw; // updates distance travled in y-direction since last reed trigger
+  distance += deltaSw; // update distance traveled since last reed trigger
+  currentPosX += distanceX; // updates the new current x-position of the robot (NEW)
+  currentPosY += distanceY; // updates the new current y-position of the robot (NEW)
+  
+  
+  float distanceCM = (distance * 2.54); //convert distance traveled from inches to cm
+  //Serial.print("Distance Traveled (in cm):"); Serial.print("\t"); Serial.println(distanceCM);
+}
+/************************* modulo for floats *******************************/
+float modulo(float x, float y) {
+  float n = floor(x/y); // find quotient rounded down
+  return x - n*y; // return the remainder of the divison
+}
+/******************** update magnetometer function ************************/
+void updateMag(void) {
+  /* Tell the HMC5883L where to begin reading data */
+  Wire.beginTransmission(address);
+  Wire.write(0x03); //select register 3, X MSB register
+  Wire.endTransmission();
+  /* Read data from each axis, 2 registers per axis */
+  Wire.requestFrom(address, 6);
+  if (6 <= Wire.available()) {
+    magX = Wire.read() << 8; //X msbs // Bitshift
+    magX |= Wire.read();    //X lsb
+    magZ = Wire.read() << 8; //Z msb
+    magZ |= Wire.read();    //Z lsb
+    magY = Wire.read() << 8; //Y msb
+    magY |= Wire.read();    //Y lsb
+  }
+
+  /* print for troubleshooting */
+    //Serial.print("X:"); Serial.print("\t"); Serial.print(magX); Serial.print("\t");
+    //Serial.print("Y:"); Serial.print("\t"); Serial.print(magY); Serial.print("\t");
+    //Serial.print("Z:"); Serial.print("\t"); Serial.print(magZ); Serial.print("\t");*/
+
+    /* Calculate magnetometer angle */
+  phi = 180. / pi * atan2(magY, magX) + thetaOffset; // convert to degrees, apply offset
+  phi = modulo(phi, 360.); // ensure that theta is between 0 and 360 degrees
+}
+
+/************************* Update Solenoid *********************************/
+void updateSolenoid(){ 
+    if ((currentTime - lastTimeSolenoid) >= solenoidInterval && solenoidState == HIGH) {
+      solenoidState = LOW;
+      digitalWrite(SolenoidPin, solenoidState); // Opens the solenoid valve
+      lastTimeSolenoid = currentTime;
+      //Serial.println("SolenoidOff");
+    } 
+    if ((currentTime - lastTimeSolenoid) >= solenoidInterval && solenoidState == LOW) {
+      solenoidState = HIGH; 
+      digitalWrite(SolenoidPin, solenoidState); // Closes the solenoid valve
+      lastTimeSolenoid = currentTime;
+      //Serial.println("SolenoidOn");
+    }
+    //Serial.println("Solenoid");
+    //Serial.print("solenoidInterval:"); Serial.print("\t"); Serial.println(solenoidInterval);
+}
+
 
 /************************* debounce interrupt ********************************************/
 void debounceInterrupt() {
@@ -186,59 +256,8 @@ void loop() {
   /* Update the sensor readings and solenoid state */
   updateMag();
   updateSolenoid(); 
-  updateServo();
-
-  Serial.print("Phi:"); Serial.print("\t"); Serial.println(phi);
-}
-
-/******************** update magnetometer function ************************/
-void updateMag(void) {
-  /* Tell the HMC5883L where to begin reading data */
-  Wire.beginTransmission(address);
-  Wire.write(0x03); //select register 3, X MSB register
-  Wire.endTransmission();
-  /* Read data from each axis, 2 registers per axis */
-  Wire.requestFrom(address, 6);
-  if (6 <= Wire.available()) {
-    magX = Wire.read() << 8; //X msbs // Bitshift
-    magX |= Wire.read();    //X lsb
-    magZ = Wire.read() << 8; //Z msb
-    magZ |= Wire.read();    //Z lsb
-    magY = Wire.read() << 8; //Y msb
-    magY |= Wire.read();    //Y lsb
-  }
-
-  /* print for troubleshooting */
-    //Serial.print("X:"); Serial.print("\t"); Serial.print(magX); Serial.print("\t");
-    //Serial.print("Y:"); Serial.print("\t"); Serial.print(magY); Serial.print("\t");
-    //Serial.print("Z:"); Serial.print("\t"); Serial.print(magZ); Serial.print("\t");*/
-
-    /* Calculate magnetometer angle */
-  phi = 180. / pi * atan2(magY, magX) + thetaOffset; // convert to degrees, apply offset //changed theta to phi!?!
-  phi = modulo(phi, 360.); // ensure that theta is between 0 and 360 degrees //changed theta to phi!?!
-}
-
-/************************* Update Solenoid *********************************/
-void updateSolenoid(){ 
-    if ((currentTime - lastTimeSolenoid) >= solenoidInterval && solenoidState == HIGH) {
-      solenoidState = LOW;
-      digitalWrite(SolenoidPin, solenoidState); // Opens the solenoid valve
-      lastTimeSolenoid = currentTime;
-      //Serial.println("SolenoidOff");
-    } 
-    if ((currentTime - lastTimeSolenoid) >= solenoidInterval && solenoidState == LOW) {
-      solenoidState = HIGH; 
-      digitalWrite(SolenoidPin, solenoidState); // Closes the solenoid valve
-      lastTimeSolenoid = currentTime;
-      //Serial.println("SolenoidOn");
-    }
-    //Serial.println("Solenoid");
-    //Serial.print("solenoidInterval:"); Serial.print("\t"); Serial.println(solenoidInterval);
-}
-
-/************************** Update Servo ***********************************/
-void updateServo() {
-  /* Sweeps the servo to check that it functions */
+  
+/* Sweeps the servo to check that it functions */
  /* Serial.print("currentTime:");   Serial.print("\t"); Serial.println(currentTime);
   Serial.print("lastTimeServo:"); Serial.print("\t"); Serial.println(lastTimeServo);
   Serial.print("servoInterval:"); Serial.print("\t"); Serial.println(servoInterval);
@@ -270,8 +289,8 @@ void updateServo() {
     servoBearing = atan((3. - (1./3.)) / (5. - .5)); //insert info for starting block 1
   }
 
-  /* update servo position */
-  servoAngle = servoBearing - phi; //where you want robot to point - where it currently points = desired change in servo angle?? I changed theta to phi...
+  /* update servo position 
+  servoAngle = servoBearing - theta; //where you want robot to point - where it currently points = desired change in servo angle
   servoAngle = modulo(servoAngle, 360.); //make sure servoAngle is between 0 and 360 degrees,*/ 
   /* does mod absolute value of #s?/take negative numbers into account? NO! See below(?). */
   /*  float turn;
@@ -283,7 +302,7 @@ void updateServo() {
    *    turn = servoBearing + 360;
    *  }
    */
-  
+  /*
   if (servoAngle > 270){ //test and adjust number to servo range
     servoAngle = 0;
   }
@@ -293,32 +312,13 @@ void updateServo() {
   
   myServo.write(servoAngle); // input can be between 0 and 180 degrees (note abovementioned changes)*/
   
- /*print for troubleshooting*/
-  // Serial.print("Phi:"); Serial.print("\t"); Serial.println(phi); //AHH! changed theta to phi.
+ /*print for troubleshooting
+  // Serial.print("Theta:"); Serial.print("\t"); Serial.println(theta);
   // Serial.print("Servo Angle:"); Serial.print("\t"); Serial.println(servoAngle);
   //Serial.print("Servo Position:"); Serial.print("\t"); Serial.println(servoPosition);
-  //Serial.print("Solenoid State:"); Serial.print("\t"); Serial.println(solenoidState);
+  //Serial.print("Solenoid State:"); Serial.print("\t"); Serial.println(solenoidState);*/
 }
 
-/************************* position estimate/distance traveled update *************************************/
-void estimate(){
-  /*assume deltaSw is >>1, therefore neglect curvature and use trig. to estimate x & y distances traveled*/
-  distanceX += cos(phi) * deltaSw; // updates distance traveled in x-direction since last reed trigger
-  distanceY += sin(phi) * deltaSw; // updates distance travled in y-direction since last reed trigger
-  distance += deltaSw; // update distance traveled since last reed trigger
-  currentPosX += distanceX; // updates the new current x-position of the robot (NEW)
-  currentPosY += distanceY; // updates the new current y-position of the robot (NEW)
-  
-  
-  float distanceCM = (distance * 2.54); //convert distance traveled from inches to cm
-  //Serial.print("Distance Traveled (in cm):"); Serial.print("\t"); Serial.println(distanceCM);
-}
-
-/************************* modulo for floats *******************************/
-float modulo(float x, float y) {
-  float n = floor(x/y); // find quotient rounded down
-  return x - n*y; // return the remainder of the divison
-}
 
 /********** signum function **********************/
 int sign(float x) {
@@ -332,24 +332,25 @@ float fix(float x) {
 }
 
 /************* Controller (TO BE CHECKED) **********************/
-/*void steering(){
+void steering(){
   if((currentPosY > -33) && (currentPosY < 33)) {// determines whether or not the robot is in the channel to run the channel control law
-  p_desiredX = currentPosX + target_lead; 
-  p_desiredY = 0;
-  phi_desired = 0; 
-  }// phi_desired was originally servoBearing; this variable refers to the desired orientation of the robot
-  else {// the statements following this else should have the robot continue to move forwards
-  p_desiredX = currentPosX;
-  p_desiredY = 0;
-  phi_desired = 90;
+    p_desiredX = currentPosX + target_lead;
+    p_desiredY = 0;
+    phi_desired = 0; 
   }
-  end
+  // phi_desired was originally servoBearing; this variable refers to the desired orientation of the robot
+  else  {// the statements following this else should have the robot continue to move forwards
+    p_desiredX = currentPosX;
+    p_desiredY = 0;
+    phi_desired = -fix(atan2(currentPosY,0));
+  }
 
-  error_pX = currentPosX - p_desiredX;
-  error_pY = currentPosY - p_desiredY;
+  error_pX = currentPosX - p_desiredX; // calculates the x-position error between the current position and the desired position
+  error_pY = currentPosY - p_desiredY; // calculates the y-position error between the current position and the desired position
+  error_p_distance = sqrt((error_pX)*(error_pX)+(error_pY)*(error_pY)); // calculates the magnitude of the error between the current position and the desired position
 
   p_angle = fix(atan2(-1*error_pY,-1*error_pX));
-  alpha_p = fix(atan2(p_angle-phi)); 
+  alpha_p = fix(p_angle-phi); 
 
   error_phi = fix(phi_desired-phi); 
 
@@ -358,11 +359,11 @@ float fix(float x) {
 
   myServo.write(90+theta_target);
 }
-*/
+
 /************** Potentiometer/Starting Position Base Code *************/
-//void start() {
-//  if (/*Insert condition for button being ON referring to starting positions in the A Block and B Block*/){ 
-   /* startingBlock = analogRead(PotPin) / 513; 
+void start() {
+  if (/*Insert condition for button being ON referring to starting positions in the A Block and B Block*/){ 
+    startingBlock = analogRead(PotPin) / 513; 
     if (startingBlock == 0){ // Position A1
       startingPosX = -99;
       startingPosY = 154;
@@ -396,9 +397,9 @@ float fix(float x) {
       startingPosY = 132;
     }
   }
-//  else if (/*Insert condition for button being OFF referring to starting*/
-//startingBlock = analogRead(PotPin) / 513;
-  /*  if (startingBlock == 0){ // Position C1
+  else if (/*Insert condition for button being OFF referring to starting positions on the C Block and D Block*/){
+    startingBlock = analogRead(PotPin) / 513;
+    if (startingBlock == 0){ // Position C1
       startingPosX = -99;
       startingPosY = -110;
     }
@@ -431,8 +432,5 @@ float fix(float x) {
       startingPosY = -132;
     }
   }
-  }*/
-
-
-  
+  }
 
